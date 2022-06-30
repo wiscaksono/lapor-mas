eb23bb1bd55620a0d3f583895c64ac215dddbacb
Wisnu Wicaksono <wwicaksono96@gmail.com>
2022-06-30 17:00:40 +0700

    up

--- frontend/pages/user/register.jsx
+++ frontend/pages/user/register.jsx
@@ -1,4 +1,38 @@
+import { useState } from "react";
+
 export default function Register() {
+  const [field, setField] = useState({});
+
+  function setValue(e) {
+    const target = e.target;
+    const name = target.name;
+    const value = target.value;
+
+    setField({
+      ...field,
+      [name]: value,
+    });
+  }
+
+  async function doRegister(e) {
+    e.preventDefault();
+
+    const req = await fetch(
+      `${process.env.NEXT_PUBLIC_URL}/auth/local/register`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(field),
+      }
+    );
+
+    const res = await req.json();
+
+    console.log(field);
+  }
+
   return (
     <div className="min-h-screen bg-background bg-cover bg-no-repeat flex justify-center items-center m-auto flex-col ">
       <h1 className="text-2xl text-primary font-semibold text-center mb-10">
@@ -6,20 +40,21 @@ export default function Register() {
         <span className="text-secondary">SCADA</span>
       </h1>
       <div className="flex items-center justify-center">
-        <form action="GET" className="w-96">
+        <form className="w-96" onSubmit={doRegister}>
           <div className="flex flex-col mb-5">
             <label
-              htmlFor="name"
+              htmlFor="username"
               className="font-semibold text-sm text-[#627D98] pl-2 mb-3"
             >
-              Name
+              Username
             </label>
             <input
-              type="name"
+              type="text"
               id="name"
-              name="name"
+              name="username"
               className="border  rounded-lg placeholder:text-[#627D98] py-3 pl-6 text-xs font-medium"
               placeholder="Name"
+              onChange={setValue}
               required
             />
           </div>
@@ -36,10 +71,11 @@ export default function Register() {
               name="email"
               className="border  rounded-lg placeholder:text-[#627D98] py-3 pl-6 text-xs font-medium"
               placeholder="Email Address"
+              onChange={setValue}
               required
             />
           </div>
-          <div className="flex flex-col mb-5">
+          {/* <div className="flex flex-col mb-5">
             <label
               htmlFor="phone"
               className="font-semibold text-sm text-[#627D98] pl-2 mb-3"
@@ -52,9 +88,10 @@ export default function Register() {
               name="phone"
               className="border  rounded-lg placeholder:text-[#627D98] py-3 pl-6 text-xs font-medium"
               placeholder="Nomor Handphone"
+              onChange={setValue}
               required
             />
-          </div>
+          </div> */}
 
           <div className="flex flex-col mb-5">
             <label
@@ -69,15 +106,19 @@ export default function Register() {
               name="password"
               className="border  rounded-lg placeholder:text-[#627D98] py-3 pl-6 text-xs font-medium"
               placeholder="Password"
+              onChange={setValue}
               required
             />
           </div>
-          <button className="py-3 text-center w-full bg-secondary hover:bg-secondary/90 transition-colors duration-150 rounded-lg text-sm text-[#FFFFFF] font-semibold mb-5 shadow-md">
+          <button
+            className="py-3 text-center w-full bg-secondary hover:bg-secondary/90 transition-colors duration-150 rounded-lg text-sm text-[#FFFFFF] font-semibold mb-5 shadow-md"
+            type="submit"
+          >
             Masuk
           </button>
           <p className="text-center text-[#627D98] text-xs font-medium">
             Sudah punya akun?{" "}
-            <a href="#" className="text-secondary  hover:underline">
+            <a href="../login" className="text-secondary  hover:underline">
               Masuk
             </a>
           </p>
