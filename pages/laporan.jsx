3ec4f1d21873addd8fd465234fcd2199a2bae0fd
wiscaksono <wwicaksono96@gmail.com>
2022-07-15 01:34:20 +0700

    up

--- pages/laporan.jsx
+++ pages/laporan.jsx
@@ -5,6 +5,8 @@ import { Transition } from "@headlessui/react";
 
 export default function Laporan() {
   const [showDetail, setShowDetail] = useState(false);
+  const [showDetail2, setShowDetail2] = useState(false);
+  const [showDetail3, setShowDetail3] = useState(false);
   return (
     <Layout className={"bg-[#F2F2F2] w-full"}>
       <div className="flex-1 flex gap-5 py-16 px-14 h-full">
@@ -70,6 +72,7 @@ export default function Laporan() {
               <div>Prioritas</div>
               <div>Status</div>
             </div>
+
             <div>
               <button
                 className="grid grid-cols-6 place-items-center py-5 text-[#627D98] font-medium text-xs w-full"
@@ -89,6 +92,264 @@ export default function Laporan() {
               </button>
               <Transition
                 show={showDetail}
+                enter="transition ease duration-100 transform"
+                enterFrom="opacity-0 -translate-y-2"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease duration-100 transform"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 -translate-y-2"
+              >
+                <div className="w-full py-8 px-11 border border-[#E4E4E7] rounded-md text-xs relative">
+                  <div className="flex items-start justify-between mb-9 ">
+                    <div className="grid grid-cols-3 font-semibold  gap-24 jadwal-expand">
+                      <div>
+                        <p className="text-[#102A43] font-semibold mb-2">
+                          Asal unit/bidang
+                        </p>
+                        <p className="text-[#627D98] font-medium">
+                          Dispatcher WPO Metro
+                        </p>
+                      </div>
+                      <div>
+                        <p className="text-[#102A43] font-semibold mb-2">
+                          Jenis Gangguan
+                        </p>
+                        <p className="text-[#627D98] font-medium">
+                          Telestatus (invalid)
+                        </p>
+                      </div>
+                      <div>
+                        <p className="text-[#102A43] font-semibold mb-2">
+                          Prioritas
+                        </p>
+                        <p className="text-[#627D98] font-medium ">Sedang</p>
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-5">
+                      <button>
+                        <img src="/icon/archive.svg" alt="" />
+                      </button>
+                      <button>
+                        <img src="/icon/trash.svg" alt="" />
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col gap-3">
+                    <div>
+                      <p className="text-[#102A43] font-semibold mb-2">
+                        GITET/GI/GIS Yang Terganggu
+                      </p>
+                      <p className="text-[#627D98] font-medium">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry&apos;s standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of type and
+                        scrambled it to make a type specimen book. It has
+                        survived not only five centuries, but also the leap into
+                        electronic typesetting, remaining essentially unchanged.
+                      </p>
+                    </div>
+                    <div>
+                      <p className="text-[#102A43] font-semibold mb-2">
+                        Penjelasan Singkat Gangguan
+                      </p>
+                      <p className="text-[#627D98] font-medium">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry.
+                      </p>
+                    </div>
+                    <div>
+                      <p className="text-[#102A43] font-semibold mb-2">
+                        Bay Yang Terganggu
+                      </p>
+                      <p className="text-[#627D98] font-medium">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry&apos;s standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of type and
+                        scrambled it to make a type specimen book. It has
+                        survived not only five centuries,
+                      </p>
+                    </div>
+                    <div>
+                      <p className="text-[#102A43] font-semibold mb-2">Bukti</p>
+                      <div className="flex items-center justify-start gap-5">
+                        <div className="aspect-video relative">
+                          <img src="bukti.png" alt="" className="w-full" />
+                          <div className="flex items-center justify-center w-full h-full bg-[#143847]/50 z-20 absolute top-0">
+                            <div className="flex flex-col items-center justify-center gap-7">
+                              <div className="flex items-center justify-center flex-col">
+                                <p className="text-sm font-semibold text-white">
+                                  foto1.jpeg
+                                </p>
+                                <p className="text-sm font-semibold text-white">
+                                  568 KB
+                                </p>
+                              </div>
+                              <img src="/icon/download.svg" alt="" />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="aspect-video">
+                          <img src="bukti.png" alt="" className="w-full" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Transition>
+            </div>
+
+            {/* second div */}
+            <div>
+              <button
+                className="grid grid-cols-6 place-items-center py-5 text-[#627D98] font-medium text-xs w-full"
+                // onClick={() => setShowDetail(true)}
+                onClick={() => setShowDetail2((showDetail2) => !showDetail2)}
+              >
+                <div>Wisnu Wicaksono</div>
+                <div>wwicaksono96@gmail.com</div>
+                <div>24.06.2022</div>
+                <div>10:23 AM</div>
+                <div className="w-[155px] h-[30px] border rounded-md text-secondary border-secondary bg-[#F2F9FF] flex items-center justify-center font-medium">
+                  Sedang
+                </div>
+                <div className="w-[155px] h-[30px] border rounded-md text-secondary border-secondary bg-[#F2F9FF] flex items-center justify-center font-medium">
+                  Sudah Diterima
+                </div>
+              </button>
+              <Transition
+                show={showDetail2}
+                enter="transition ease duration-100 transform"
+                enterFrom="opacity-0 -translate-y-2"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease duration-100 transform"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 -translate-y-2"
+              >
+                <div className="w-full py-8 px-11 border border-[#E4E4E7] rounded-md text-xs relative">
+                  <div className="flex items-start justify-between mb-9 ">
+                    <div className="grid grid-cols-3 font-semibold  gap-24 jadwal-expand">
+                      <div>
+                        <p className="text-[#102A43] font-semibold mb-2">
+                          Asal unit/bidang
+                        </p>
+                        <p className="text-[#627D98] font-medium">
+                          Dispatcher WPO Metro
+                        </p>
+                      </div>
+                      <div>
+                        <p className="text-[#102A43] font-semibold mb-2">
+                          Jenis Gangguan
+                        </p>
+                        <p className="text-[#627D98] font-medium">
+                          Telestatus (invalid)
+                        </p>
+                      </div>
+                      <div>
+                        <p className="text-[#102A43] font-semibold mb-2">
+                          Prioritas
+                        </p>
+                        <p className="text-[#627D98] font-medium ">Sedang</p>
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-5">
+                      <button>
+                        <img src="/icon/archive.svg" alt="" />
+                      </button>
+                      <button>
+                        <img src="/icon/trash.svg" alt="" />
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col gap-3">
+                    <div>
+                      <p className="text-[#102A43] font-semibold mb-2">
+                        GITET/GI/GIS Yang Terganggu
+                      </p>
+                      <p className="text-[#627D98] font-medium">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry&apos;s standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of type and
+                        scrambled it to make a type specimen book. It has
+                        survived not only five centuries, but also the leap into
+                        electronic typesetting, remaining essentially unchanged.
+                      </p>
+                    </div>
+                    <div>
+                      <p className="text-[#102A43] font-semibold mb-2">
+                        Penjelasan Singkat Gangguan
+                      </p>
+                      <p className="text-[#627D98] font-medium">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry.
+                      </p>
+                    </div>
+                    <div>
+                      <p className="text-[#102A43] font-semibold mb-2">
+                        Bay Yang Terganggu
+                      </p>
+                      <p className="text-[#627D98] font-medium">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry&apos;s standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of type and
+                        scrambled it to make a type specimen book. It has
+                        survived not only five centuries,
+                      </p>
+                    </div>
+                    <div>
+                      <p className="text-[#102A43] font-semibold mb-2">Bukti</p>
+                      <div className="flex items-center justify-start gap-5">
+                        <div className="aspect-video relative">
+                          <img src="bukti.png" alt="" className="w-full" />
+                          <div className="flex items-center justify-center w-full h-full bg-[#143847]/50 z-20 absolute top-0">
+                            <div className="flex flex-col items-center justify-center gap-7">
+                              <div className="flex items-center justify-center flex-col">
+                                <p className="text-sm font-semibold text-white">
+                                  foto1.jpeg
+                                </p>
+                                <p className="text-sm font-semibold text-white">
+                                  568 KB
+                                </p>
+                              </div>
+                              <img src="/icon/download.svg" alt="" />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="aspect-video">
+                          <img src="bukti.png" alt="" className="w-full" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Transition>
+            </div>
+
+            {/* third div */}
+            <div>
+              <button
+                className="grid grid-cols-6 place-items-center py-5 text-[#627D98] font-medium text-xs w-full"
+                // onClick={() => setShowDetail(true)}
+                onClick={() => setShowDetail3((showDetail3) => !showDetail3)}
+              >
+                <div>Wisnu Wicaksono</div>
+                <div>wwicaksono96@gmail.com</div>
+                <div>24.06.2022</div>
+                <div>10:23 AM</div>
+                <div className="w-[155px] h-[30px] border rounded-md text-secondary border-secondary bg-[#F2F9FF] flex items-center justify-center font-medium">
+                  Sedang
+                </div>
+                <div className="w-[155px] h-[30px] border rounded-md text-secondary border-secondary bg-[#F2F9FF] flex items-center justify-center font-medium">
+                  Sudah Diterima
+                </div>
+              </button>
+              <Transition
+                show={showDetail3}
                 enter="transition ease duration-100 transform"
                 enterFrom="opacity-0 -translate-y-2"
                 enterTo="opacity-100 translate-y-0"
